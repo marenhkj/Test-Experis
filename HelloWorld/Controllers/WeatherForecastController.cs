fd6e7d357b4e27a58da1da68f31e0d02f025514a
Maren Kjensli <maren.kjensli@no.experis.com>
2023-06-29 13:54:01 +0200

    Controllers

--- HelloWorld/Controllers/WeatherForecastController.cs
+++ HelloWorld/Controllers/WeatherForecastController.cs
@@ -18,8 +18,21 @@ namespace HelloWorld.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpPost("PostWeatherForecast")]
+        public IEnumerable<WeatherForecast> Post()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
