3c45986287c85a7ff10eee81d719108f5eec5163
Maren Kjensli <maren.kjensli@no.experis.com>
2023-06-29 13:02:52 +0200

    comment

--- HelloWorld/WeatherForecast.cs
+++ HelloWorld/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace HelloWorld
+namespace HelloWorld 
 {
     public class WeatherForecast
     {
@@ -8,6 +8,8 @@ namespace HelloWorld
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary { get; set; }      
     }
 }
+
+// Kommentar
